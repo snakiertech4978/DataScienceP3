f3059a68d415ccc95ce98f92289a3a4aba7dcaa2
snakiertech4978 <52748188+snakiertech4978@users.noreply.github.com>
2023-03-27 15:30:45 +0200

    Dag 1 15.30

---
+++ 2.Werken met Python/Hulpfuncties.py
@@ -0,0 +1,29 @@
+import math
+"Simpele wiskundige berekeningen"
+"_______________________________________________________________________________________________________________________"
+"Vermenigvuldigen"
+print(7 * 6)
+"Machtsverheffing"
+print(2 ** 3)
+"haakjes"
+print((2+4)*(9-2))
+"Goneometrische functies: hier 60 keer de sinus van pi op 4"
+print(60 * math.sin(math.pi / 4))
+"_______________________________________________________________________________________________________________________"
+"Soorten variabelen"
+a = "bla"
+print((type(a)))
+a = 42.5
+print((type(a)))
+a = True
+print((type(a)))
+"Getal dat niet een integer vormt tot een integer omvormen"
+mijn_variabele = 127 / 3
+print(mijn_variabele)
+mijn_variabele = 127 // 3
+print(mijn_variabele)
+"_______________________________________________________________________________________________________________________"
+
+
+
+
